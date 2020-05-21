88f9be515287d02873465166b54258835e92f2dd
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-05-21 15:44:23 +0530

    changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //import { MatSliderModule } from '@angular/material/slider';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule  } from '@angular/material/input';
 import { EmployeeComponent } from './employee/employee.component';
 import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
 import { EditEmployeeComponent } from './employee/edit-employee/edit-employee.component';
 import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
 import { DepartmentComponent } from './department/department.component';
-import { ShowDepartmentComponent } from './employee/show-department/show-department.component';
 import { EditDepartmentComponent } from './department/edit-department/edit-department.component';
 import { AddDepartmentComponent } from './department/add-department/add-department.component';
+import { DepartmentService } from 'src/app/services/department.service'
+import { EmployeeService } from 'src/app/services/employee.service'
 
 @NgModule({
   declarations: [
@@ -24,16 +25,18 @@ import { AddDepartmentComponent } from './department/add-department/add-departme
     EditEmployeeComponent,
     AddEmployeeComponent,
     DepartmentComponent,
-    ShowDepartmentComponent,
     EditDepartmentComponent,
-    AddDepartmentComponent
+    AddDepartmentComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     //MatSliderModule,
-    MatInputModule
+    MatInputModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
